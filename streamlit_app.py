c70a00e72490f08878fe7cc035f0ffa4b1e7277d
Andrew Shiroma <andrew.shiroma@saddleback.com>
2024-12-17 12:03:05 -0800

    changed template openai to groq

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,59 @@
 import streamlit as st
-from openai import OpenAI
+from groq import Groq
 
 # Show title and description.
-st.title("💬 Chatbot")
-st.write(
-    "This is a simple chatbot that uses OpenAI's GPT-3.5 model to generate responses. "
-    "To use this app, you need to provide an OpenAI API key, which you can get [here](https://platform.openai.com/account/api-keys). "
-    "You can also learn how to build this app step by step by [following our tutorial](https://docs.streamlit.io/develop/tutorials/llms/build-conversational-apps)."
-)
+# st.title("💬 Chatbot")
+# st.write(
+#     "A simple chatbot for R&I Volunteer and Intern Assistance."
+# )
 
 # Ask user for their OpenAI API key via `st.text_input`.
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
-openai_api_key = st.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Please add your OpenAI API key to continue.", icon="🗝️")
-else:
+# openai_api_key = st.text_input("OpenAI API Key", type="password")
+# if not openai_api_key:
+#     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
+# else:
 
-    # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+groq_client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+pinecone_client = Pinecone(api_key=st.secrets["PINECONE_API_KEY"])
+pinecone_index = pinecone_client.Index("ri-assist")
+pinecone_namespace = ""
 
-    # Create a session state variable to store the chat messages. This ensures that the
-    # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+# Create a session state variable to store the chat messages. This ensures that the messages persist across reruns.
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-    # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+# Display the existing chat messages via `st.chat_message`.
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-    # Create a chat input field to allow the user to enter a message. This will display
-    # automatically at the bottom of the page.
-    if prompt := st.chat_input("What is up?"):
+# Create a chat input field to allow the user to enter a message at bottom of page
+if prompt := st.chat_input("How can I help?"):
 
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
+    # embed the prompt, query the pinecone database, and create a llm-friendly query 
+    prompt_embed = SentenceTransformer("sentence-transformers/all-mpnet-base-v2").encode(prompt)
+    pinecone_matches = pinecone_index.query(vector=query_embed.tolist(), top_k=5, include_metadata=True, namespace=pinecone_namespace)
+    contexts = [match['metadata']['text'] for match in pinecone_matches['matches']]
+    augmented_prompt = "<CONTEXT>\n" + "\n-------\n".join(contexts[:10]) + "\n-------\n</CONTEXT>\n\nMY QUESTION:\n" + query
 
-        # Generate a response using the OpenAI API.
-        stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
+    # Store and display the current prompt.
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+    # Generate a response.
+    stream = groq_client.chat.completions.create(
+        model="llama-3.1-70b-versatile",
+        messages=[
+            {"role": m["role"], "content": m["content"]}
+            for m in st.session_state.messages
+        ],
+        stream=True,
+    )
+
+    # Stream the response to the chat using `st.write_stream`, then store it in session
+    with st.chat_message("assistant"):
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
